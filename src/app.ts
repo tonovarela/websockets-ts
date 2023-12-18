440cf228764c2c2794122e3bee16c2a2f36355a6
tonovarela <tonovarela@live.com>
2023-12-18 09:07:05 -0600

    Fin de la seccion

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
-import { WebSocketServer } from 'ws';
+import {WebSocket, WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 3000 });
 wss.on('connection', function connection(ws) {
-    console.log('Client connected');
-    ws.on('error', console.error);
-    ws.on('close', ()=>{
-     console.log('Client disconnected');
+  console.log('Client connected');
+  ws.on('error', console.error);
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+  ws.on('message', function message(data) {
+    const payload = {
+      type: 'custom-message',
+      payload: data.toString().toUpperCase()
+    };
+    //Todos incluyentes
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(JSON.stringify(payload), { binary: false });
+    //   }
+    // });
+
+    //Todos excluyentes
+    wss.clients.forEach(function each(client) {
+      if (client!== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(payload), { binary: false });
+      }
     });
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-    });
-    setInterval(()=>{
-      ws.send('Hola de nuevo');
-    },2000)
+    
+    
+  });
 });
